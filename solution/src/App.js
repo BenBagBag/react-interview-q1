389317f9330b83fdbdf4fa84e978eaae54dd3782
Isaac Cowhey <isaaccowhey@Isaacs-MacBook-Air.local>
2024-04-12 07:45:23 -0700

    add comments

--- solution/src/App.js
+++ solution/src/App.js
@@ -11,12 +11,14 @@ import { isNameValid, getLocations } from './mock-api/apis';
 import './App.css';
 
 function App() {
+  // Names stored in state because this isn't a DB assignment :)
   const [namesList, setNamesList] = useState([]);
   const [currentNameInput, setCurrentNameInput] = useState('');
   const [isValidCurrentNameInput, setIsValidCurrentNameInput] = useState(true);
   const [locationsList, setLocationsList] = useState([]);
   const [location, setLocation] = useState('');
 
+  // Fetch list of locations when page loads
   useEffect(() => {
     const locationsAsync = async () => {
       try {
@@ -30,6 +32,7 @@ function App() {
     locationsAsync();
   }, [])
 
+  // Check if the name is valid (according to the API)
   const validateName = async (nameInput) => {
     try {
       const valid = await isNameValid(nameInput);
@@ -39,22 +42,29 @@ function App() {
     }
   }
 
+  // Create debounced version of validation function
+  // So you don't check every split second
   const debouncedValidateName = debounce((nameInput) => {
     validateName(nameInput);
   }, 100);
 
+  // Handle name update
+  // Update NameInput and validate name
   const onUpdateNameInput = (e) => {
-    e.preventDefault();
     const nameInput = e.target.value;
     setCurrentNameInput(nameInput);
     debouncedValidateName(nameInput);
   }
 
+  // Clear input
   const onClear = () => {
     setCurrentNameInput('');
     setLocation('');
   }
 
+  // Handle clicking 'Add button'
+  // Add new person to the NamesList as a dictionary with name and location
+  // Clear input so user can add another person
   const onAdd = () => {
     let currentNamesList = namesList;
     currentNamesList.push({
